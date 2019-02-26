d39ed24be52655cfb54a42f0d9f8c68a93c4c78c
wenzhe-zheng <wzheng41@asu.edu>
2019-02-26 10:02:49 +0800

    cseRepository Init Class Calculator in package cse360assign2

---
+++ Calculator.java
@@ -0,0 +1,37 @@
+
+package cse360assign2;
+
+//class for basic calculation
+public class Calculator {
+
+	private int total;
+	
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//return the final result
+	public int getTotal () {
+		return 0;
+	}
+	//perform add operation
+	public void add (int value) {
+		
+	}
+	//perform sub operation
+	public void subtract (int value) {
+		
+	}
+	//perform multiply operation
+	public void multiply (int value) {
+		
+	}
+	//perform divide operation
+	public void divide (int value) {
+		
+	}
+	//get the history of all the operation
+	public String getHistory () {
+		return "";
+	}
+}
